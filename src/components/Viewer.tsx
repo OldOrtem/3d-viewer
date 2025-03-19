ddb2a4dce7d18c0017cf45affe1c81ae36413807
ARTEM <artemsmirnof@mail.ru>
2025-03-19 19:53:30 +0300

    add show grid

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
@@ -6,41 +6,57 @@ import { shapeStore } from '../stores/ShapeStore';
 import { ShapeType } from '../models/Shapes';
 import BoxMesh from './BoxMesh';
 import PyramidMesh from './PyramidMesh';
+import { Checkbox } from 'antd';
 
 const Viewer: React.FC = observer(() => {
+	const [showGrid, setShowGrid] = useState<boolean>(false);
 	return (
-		<Canvas camera={{ position: [10, 10, 20], fov: 50 }}>
-			<ambientLight intensity={0.8} />
-			<directionalLight position={[5, 5, 5]} intensity={1.5} />
-			<pointLight position={[10, 20, 10]} />
+		<>
+			<div className='showgrid'>
+				<Checkbox
+					checked={showGrid}
+					onChange={(e) => setShowGrid(e.target.checked)}
+				>
+					Show grid
+				</Checkbox>
+			</div>
+			<Canvas camera={{ position: [10, 10, 20], fov: 50 }}>
+				<ambientLight intensity={0.8} />
+				<directionalLight position={[5, 5, 5]} intensity={1.5} />
+				<pointLight position={[10, 20, 10]} />
 
-			<OrbitControls />
+				<OrbitControls />
 
-			<gridHelper args={[20, 20]} />
+				{showGrid && (
+					<>
+						<gridHelper args={[20, 20]} />
 
-			<axesHelper args={[10]} />
+						<axesHelper args={[10]} />
+					</>
+				)}
 
-			{shapeStore.shapes.map((shape) => {
-				const isSelected = shapeStore.selectedShapeId === shape.id;
+				{shapeStore.shapes.map((shape) => {
+					const isSelected = shapeStore.selectedShapeId === shape.id;
 
-				switch (shape.type) {
-					case ShapeType.BOX:
-						return (
-							<BoxMesh key={shape.id} shape={shape} isSelected={isSelected} />
-						);
-					case ShapeType.PYRAMID:
-						return (
-							<PyramidMesh
-								key={shape.id}
-								shape={shape}
-								isSelected={isSelected}
-							/>
-						);
-					default:
-						return null;
-				}
-			})}
-		</Canvas>
+					switch (shape.type) {
+						case ShapeType.BOX:
+							return (
+								<BoxMesh key={shape.id} shape={shape} isSelected={isSelected} />
+							);
+						case ShapeType.PYRAMID:
+							return (
+								<PyramidMesh
+									key={shape.id}
+									shape={shape}
+									isSelected={isSelected}
+								/>
+							);
+						default:
+							return null;
+					}
+				})}
+			</Canvas>
+		</>
 	);
 });
 
